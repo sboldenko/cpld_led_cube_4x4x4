031bc1bf0f322a73b090aa4a4a9a257005875a53
BSN <electrosboldenko@gmail.com>
2021-03-25 18:54:39 +0300

    Поправлены отступы в топовом файле.

--- cpld_xc9572xl_module/hdl/vhdl/top_cpld_xc9572xl_module.vhd
+++ cpld_xc9572xl_module/hdl/vhdl/top_cpld_xc9572xl_module.vhd
@@ -11,58 +11,58 @@ use unisim.vcomponents.all;
 entity top_cpld_xc9572xl_module is
 port
 (
-	clk_system : in  std_logic;
-	aresetn    : in  std_logic;
-	stage      : out std_logic_vector( 3 downto 0);
-	lines      : out std_logic_vector(15 downto 0);
-	m_sel      : in  std_logic;
-	comp_en    : in  std_logic;
-	comp       : in  std_logic;
-	status     : out std_logic
+    clk_system : in  std_logic;
+    aresetn    : in  std_logic;
+    stage      : out std_logic_vector( 3 downto 0);
+    lines      : out std_logic_vector(15 downto 0);
+    m_sel      : in  std_logic;
+    comp_en    : in  std_logic;
+    comp       : in  std_logic;
+    status     : out std_logic
 );
 end entity;
 
 architecture behavioral of top_cpld_xc9572xl_module is
 	
-	component leds_control
-	port
-	(
-		clk     : in  std_logic;
-		reset   : in  std_logic;
-		m_sel   : in  std_logic;
-		comp_en : in  std_logic;
-		comp    : in  std_logic;          
-		stage   : out std_logic_vector( 3 downto 0);
-		lines   : out std_logic_vector(15 downto 0);
-		status  : out std_logic
-	);
-	end component;
-	
-	signal clk   : std_logic;
-	signal reset : std_logic;
+    component leds_control
+    port
+    (
+        clk     : in  std_logic;
+        reset   : in  std_logic;
+        m_sel   : in  std_logic;
+        comp_en : in  std_logic;
+        comp    : in  std_logic;          
+        stage   : out std_logic_vector( 3 downto 0);
+        lines   : out std_logic_vector(15 downto 0);
+        status  : out std_logic
+    );
+    end component;
+    
+    signal clk   : std_logic;
+    signal reset : std_logic;
 	
 begin
 	
     BUFG_inst : BUFG
     port map 
-	(
+    (
         O => clk,     
         I => clk_system 
     );
 	
 	reset <= not aresetn;
 	
-	leds_control_inst: leds_control 
-	port map
-	(
-		clk     => clk,
-		reset   => reset,
-		stage   => stage,
-		lines   => lines,
-		m_sel   => m_sel,
-		comp_en => comp_en,
-		comp    => comp,
-		status  => status
-	);
+    leds_control_inst: leds_control 
+    port map
+    (
+        clk     => clk,
+        reset   => reset,
+        stage   => stage,
+        lines   => lines,
+        m_sel   => m_sel,
+        comp_en => comp_en,
+        comp    => comp,
+        status  => status
+    );
 	
 end behavioral;
